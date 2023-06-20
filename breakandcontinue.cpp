64d346c4796c0c97ac366d5cd4bede8f41dc9ac8
Ashnuta <ashnuta.0401@gmail.com>
2023-06-21 01:08:40 +0530

    break and continue in c++

---
+++ breakandcontinue.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // int n=5;
+    // for(int i=0; i<n; i++)
+    // {
+    //     cout<< "Uppu"<< endl;
+    //     break;
+    // }
+    // cout<<  "Ashnu"<< endl;
+
+    for(int i=0; i<5; i++)
+    {
+        continue;
+        cout<< i<< endl;
+    }
+    return(0);
+}
+
+
+    
