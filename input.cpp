5917c72b66062703e9f5f4ea350a264d06380f0d
Ashnuta <ashnuta.0401@gmail.com>
2023-06-21 01:03:28 +0530

    taking input in c++

---
+++ input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Enter a number:" << endl;
+    
+    int a;
+    //taking input
+    cin >> a;
+    cout << "You entered " << a << endl;
+}
