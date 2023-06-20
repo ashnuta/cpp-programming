59cecf170c84aec2bb01ed40e42dee3716b3eed2
Ashnuta <ashnuta.0401@gmail.com>
2023-06-21 01:05:18 +0530

    varables and datatypes in c++

---
+++ variablesndt.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    //Explicit Typecasting
+    // double d=5.75;
+    // int x = (int)d +2; //takes floor value
+    // cout << "Vaue of x is " << x << endl;
+    
+    // Implicit Typecasting
+    // char ch = 97;
+    // cout << ch << endl;
+
+    // int num = 'b';
+    // cout << num << endl;
+
+    // bool a = 5;
+    // cout << sizeof(a) << endl;
+
+    // float b = 5;
+    // cout << sizeof(b) << endl;
+
+    // double c = 5;
+    // cout << sizeof(c) << endl;
+
+    // long d = 5;
+    // cout << sizeof(d) << endl;
+
+
+    // int a = 5;
+    // cout << "Value of a is:" << a <<endl;
+
+    // char ch = '@';
+    // cout << "Value of ch is:"<< ch << endl; 
+    
+    // bool flag = 1;
+    // cout << "Value of flag is" << flag << endl;
+
+
+
+   return 0;
+}
