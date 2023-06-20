88ad0a497a7d8d1b89fadd6805bcf37c48dfceff
Ashnuta <ashnuta.0401@gmail.com>
2023-06-21 01:02:57 +0530

    operators in c++

---
+++ operators.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//     int a = 5;
+//     int b = 3;
+// //Arithmetic Operators
+//     cout << a+b << endl;
+//     cout << a-b << endl;
+//     cout << a*b << endl;
+//     cout << a%b << endl;
+//     cout << a/b << endl;
+    
+//Relational operators
+
+    // int a = 5;
+    // int b = 3;
+
+    // cout << (a>b) << endl;
+    // cout << (a<b) << endl;
+    // cout << (a>=b) << endl;
+    // cout << (a<=b) << endl;
+    // cout << (a!=b) << endl;
+    // cout << (a==b) << endl;
+
+//Logical Operators
+    // int a = 5;
+    // int b = 3;
+
+    // cout << (a>=5 && b<=3) << endl;
+    // cout << (a<5 && b>3) << endl;
+    // cout << (a>5 && b>=3) << endl;
+    // cout << (a>5 || b>=3) << endl;
+    // cout << (a>5) << endl;
+    // cout << !(a==5) << endl;
+
+
+//bitwise operators
+
+    // bool a= true;
+    // bool b= true;
+    // bool a= false;
+    // bool b= false;
+
+    // cout<< (a|b);
+    // cout<< ~(a);
+    // cout<< (a^b);
+
+    // cout<< (5&10)<< endl;
+
+//left and right shift operator
+//Left shift
+    // int a = 12;
+    // a= a<<1;
+    // a= a<<2;
+    // a=a<<-1;  //kuchh compilers mein it shows garbage value neg left shift nhi hota 
+    // cout<< a<< endl;
+
+//Right shift
+    // int a=-12;
+    // a= a>>1;
+    // cout<< a<< endl;
+
+//Pre and post increment
+    // int a= 11;
+    // cout<< ++a<< endl;
+    // cout<< a++;
+    // cout<< endl<< a<< endl;
+
+    // int a =6;
+    // int c= ++a +1;
+    // cout<< c<< endl;
+
+    int a=5;
+    cout<< (++a) * (++a)<< endl; 
+    return (0);
+}
