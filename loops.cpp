75d196e8b91ddde52fcec8af74721236f10f917a
Ashnuta <ashnuta.0401@gmail.com>
2023-06-21 01:01:45 +0530

    loops in c++

---
+++ loops.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    // for(int i=0;i<5;i=i+1)
+    // {
+    //    cout<< "Ashnu Uppu" << endl; 
+    // }
+
+    // for(int i=0;i<5;i++)
+    // {
+    //     cout << i << endl; 
+    // }
+
+    // for(int i=5; i>0; i--)
+    // {
+    //     cout << i << endl;
+    // }
+
+    // for(int i=0;i<=10;i++)
+    // {
+    //     cout<<2*i<<endl;
+    // }
+   
+
+
+// Dry run of the above
+// 0<=10 true     1<=10 true       2<=10 true          10<=10 true
+// print 2*0=0     print 2*1=2     print 2*2=4 ...     print 2*10=20
+// output 0,2,4,6,8,10,12,14,16,18,20
+
+//     for(int i=1; i<=5; i=i+2)
+//     {
+//         cout<<i<<endl;
+//     }
+
+//  Dry run of above
+//  1<=5 true     3<=5 true       5<=5 true        7<=5  
+//  print 1        print 3        print 5          False    
+//  i=2+1=3        i=2+3=5        i=2+5=7
+    
+//     for(int i=1; i<10; i=i*2)
+//     {
+//         cout<<i<<endl;
+//     }
+    
+//     for(int i=100; i>0 ; i=i/2)
+//     {
+//         cout<<i<<endl;
+//     }
+
+    // Dry run for the above
+    // 100>0 true     50>0 true            25>0 true          12>10 true   6>0 true
+    // print 100       print 50            print 25           print 12     print 6
+    // i=100/2=50      i=50/25             i=25/2=12          i=12/2=6     i=6/2=3... 3/2=1...1
+
+
+    // for (int i=5; (i>=0 && i<=10);i++){
+    //     cout<<i<<endl;
+    // }
+
+    // another way of writing the code
+        // int i=0;
+        // for( ; ;){
+        //     if(i<5)
+        //     {
+        //         cout<<i<<endl;
+        //         i++;
+        //     }
+            
+        // }
+
+    // int n;
+    // if(cin>>n){
+    //     cout<<"Ash"<<endl;
+    // }
+
+    if(cout<<"Ash"<<endl){
+        cout<<"Ashnu";
+    }
+    return 0;
+    
+}
