dffae8cb80668dd81de3863a776e4ab8f5062bf1
Ashnuta <ashnuta.0401@gmail.com>
2023-06-21 01:15:57 +0530

    more cpp files

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+int main(){
+
+//     // int n;
+//     // cout<< "Enter the value of n"<< endl;
+//     // cin>> n;
+
+//     // for(int i=0; i<n; i++){
+//     //     cout<< "uppu"<< endl;
+//     // }
+//     //yeh agar hume baar baar arwana ho toh copy paste ya bohot lengthy and bulky isliye functions 
+// }
