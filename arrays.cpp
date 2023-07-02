4207cbde08871db28fd66ea3717dd46e8bb45b3e
Ashnuta <ashnuta.0401@gmail.com>
2023-07-02 16:54:14 +0530

    arrays in cpp

---
+++ arrays.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //array declare 
+
+    // int arr[7];
+    // cout<< "Array created successfully"<< endl;
+    // //address print 
+    // cout<< arr<< endl;
+    // cout<< &arr<< endl; //answer same aata hai both show base addresses 
+    // return 0;
+
+    //array initialization
+    // int arr[]= {2,3,4,6,8};
+    // int brr[5]={2,3,4,5,};
+    // int crr[10]={2,4,5,6,8}; //baaki jagah 0 store hua hoga
+    // //int drr[4]= {2,4,5,6,7,8}; // error
+    // cout<< "Array initialized successfully"<< endl;
+
+    //Accessing array elements
+
+    // int arr[]={2,4,5,6,7};
+    // cout<< arr[0]<< endl;
+    // //printing all elements
+    // for(int i=0; i<5;i++){
+    //     cout << arr[i]<<" ";
+    // }
+//logic of the loop
+    // i=0              i=1
+    // 0<5= true        i<5=true
+    // print arr[0]     print arr[1]
+    // i++              i++   and so on
+
+    //taking input for the elements of the array
+
+    // int arr[10];
+    // cout<<"Enter the input values of the array"<< endl;
+
+    // for(int i=0; i<10; i++){
+    //     cin>>arr[i]; 
+    //     // or int n;
+    //     // cin>> n;
+    //     // arr[i]=n;
+    // }
+
+    // //printing the array
+    // cout<< "printing the array"<< endl;
+    // for(int i=0; i<10; i++){
+    //     cout<< arr[i] << " ";
+    // }
+
+    return 0;
+}
