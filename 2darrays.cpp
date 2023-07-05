c7dbc337bf649731b9d26ff680e5551d8d7adb67
Ashnuta <ashnuta.0401@gmail.com>
2023-07-06 01:05:27 +0530

    2D arrays in cpp

---
+++ 2darrays.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    //declare 2D array
+    // int arr[3][3];
+
+    //initialization
+    // int brr[3][3]={
+    // {1,2,3},
+    // {4,5,6},
+    // {7,8,9}};
+
+    // cout<< brr[2][2]<< endl;
+
+    //row-wise print
+    //outer loop
+    // for(int i=0; i<3;i++){
+    //     //for ever row we need to print value in each column
+    //     for(int j=0; j<3; j++){
+    //         cout<< brr[i][j]<< " ";
+    //     }
+    //     cout<< endl;
+    // }
+
+    //col-wise print
+    //outer loop
+    // for(int i=0; i<3;i++){
+    //     //for ever row we need to print value in each column
+    //     for(int j=0; j<3; j++){
+    //         cout<< brr[j][i]<< " ";
+    //     }
+    //     cout<< endl;
+    // }
+
+    // int arr[3][3];
+    // int rows = 3;
+    // int cols = 3;
+
+    // row- wise input
+    // for(int i=0; i<4; i++){
+    //     for(int j=0; j<3; j++){
+    //         cin>> arr[i][j];
+    //     }
+    // }
+
+    // cout<<"prinitng row-wise"<< endl;
+    // for(int i=0; i<4; i++){
+    //     for(int j=0; j<3; j++){
+    //         cout<< arr[i][j]<< " ";
+    //     }
+    //     cout <<endl ;
+    // }
+
+    //jab input hi col wise le rhe toh normal print karenge
+    // for(int i=0; i<3; i++){
+    //     for(int j=0; j<3; j++){
+    //         cin>> arr[j][i];
+    //     }
+    // }
+
+    // cout<<"prinitng col-wise"<< endl;
+    // for(int i=0; i<3; i++){
+    //     for(int j=0; j<3; j++){
+    //         cout<< arr[i][j]<< " ";
+    //     }
+    //     cout <<endl ;
+    // }
+
+
+}
