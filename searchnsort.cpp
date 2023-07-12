43fd7ce622d6ba5070f43401946c2c485056e0c1
Ashnuta <ashnuta.0401@gmail.com>
2023-07-12 23:46:58 +0530

    binary search in cpp

--- searchnsort.cpp
+++ searchnsort.cpp
@@ -1,44 +1,68 @@
 #include<iostream>
+#include<algorithm> // for the predefined functions
+#include<vector>
 using namespace std;
 
-int binarySearch(int arr[], int size, int target){
-    int start = 0;
-    int end= size-1;
+// int binarySearch(int arr[], int size, int target){
+//     int start = 0;
+//     int end= size-1;
 
-    int mid = (start+end) / 2; 
+//     int mid = start + (end - start) / 2; 
 
-    while (start <= end){
-        int element = arr[mid];
+//     while (start <= end){
+//         int element = arr[mid];
 
-        if(element == target){
-            return mid;
-        }
-        else if (target< element){
-             //search in left
-            end = mid-1;
-        }
-        else{
-            start = mid+1;
-        }
-        mid = (start + end) / 2;
-    }
-    //element not found
-    return -1;
-}
+//         if(element == target){
+//             return mid;
+//         }
+//         else if (target< element){
+//              //search in left
+//             end = mid-1;
+//         }
+//         else{
+//             //search in right  
+//             start = mid+1;
+//         }
+//         mid = start + (end - start) / 2; 
+
+//     }
+//     //element not found
+//     return -1;
+// }
 
 
-int main(){
-    int arr[] = {2,4,6,8,10,12,16};
-    int size = 7;
-    int target = 2;
+// int main(){
+//     int arr[] = {2,4,6,8,10,12,16};
+//     int size = 7;
+//     int target = 2;
 
-    int indexOftarget = binarySearch(arr,size,target);
+//     int indexOftarget = binarySearch(arr,size,target);
 
-    if(indexOftarget == -1){
-        cout<< "target not found"<< endl;
-    }
-    else{
-        cout<< "target found at "<< indexOftarget<< " index "<< endl;
-    }
-    return 0;  
-}
+//     if(indexOftarget == -1){
+//         cout<< "target not found"<< endl;
+//     }
+//     else{
+//         cout<< "target found at "<< indexOftarget<< " index "<< endl;
+//     }
+//     return 0;  
+// }
+
+
+//inbuilt/ predefined binary search function
+
+// int main(){
+
+//     vector <int> v{1,2,3,4,5,6,7};
+//     int arr[]= {1,2,3,4,5,6,7};
+//     int size = 7;
+
+//     // if (binary_search(v.begin(), v.end(), 3)){ for 
+//     if (binary_search(arr,arr +size,7)){
+//         cout<< "Found" << endl;
+//     }
+//     else{
+//         cout<<"Not Found"<<endl;
+//     }
+//     return 0;
+// }
+
